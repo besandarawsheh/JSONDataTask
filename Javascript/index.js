719cd0d5e916b5d6262f6fef52e389245b09258f
Besna <bisan.darawsheh@gmail.com>
2019-09-01 16:43:51 +0300

    again i forget to commit changes

--- Javascript/index.js
+++ Javascript/index.js
@@ -7,31 +7,37 @@ window.addEventListener('load', function() {
 const apiURL="https://reqres.in/api/users";
 async function showData (){
     let users = await getUsersList("GET",`${apiURL}?per_page=4`);
-    let allUsers=await getUsersList("GET",`${apiURL}?per_page=${total}`);
-
+    //let allUsers=await getUsersList("GET",`${apiURL}?per_page=12`);
     let usersObj=JSON.parse(users).data;
-    //console.log(usersObj.length);
-    //console.log(usersObj[0].id);
+    //let allUsersList=JSON.parse(allUsers).data;
     let usersList= document.getElementById("users-list");
     let usersTable= document.getElementById("usersTableBody");
-
+    // for (i in allUsersList){
+    //     let element =document.createElement('li');
+    //     let userfullName =document.createElement('span');
+    //     let userImg = document.createElement('img');
+    //     userImg.src=allUsersList[i].avatar;
+    //     userfullName.textContent=`${allUsersList[i].first_name} ${allUsersList[i].last_name}`;
+    //     element.append(userImg,userfullName);
+    //     usersList.appendChild(element);
+    // }
     for (x in usersObj) {
+        ///fill usersList with data
         let element =document.createElement('li');
         let userfullName =document.createElement('span');
         let userImg = document.createElement('img');
         userImg.src=usersObj[x].avatar;
         userfullName.textContent=`${usersObj[x].first_name} ${usersObj[x].last_name}`;
         element.append(userImg,userfullName);
+        usersList.appendChild(element);
+        ///Fill table with data
         let row =document.createElement('tr');
         let col1 = document.createElement('td');
         let col2 = document.createElement('td');
         let col3 = document.createElement('td');
-
-        //element.textContent=usersObj[x].email;
         col1.textContent=usersObj[x].email;
         col2.textContent=usersObj[x].first_name;
         col3.textContent=usersObj[x].last_name;
-        usersList.appendChild(element);
         row.append(col1,col2,col3);
         usersTable.appendChild(row);
     }
@@ -39,7 +45,6 @@ async function showData (){
 
 
 function getUsersList (method,url){
-
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url);
